3f182e2701e550b0b28640dfa8864ec36fb3b414
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-24 20:30:15 +0800

    feat(custom): to configure the env for pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { setupPinia } from './store';
+
 function boot() {
   const app = createApp(App);
+
+  setupPinia(app);
 
   app.mount('#app');
 }
